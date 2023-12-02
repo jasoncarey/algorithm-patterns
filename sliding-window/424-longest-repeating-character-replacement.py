6929bccfa35581da66d25394550fd48d362de60e
Jason Carey <jasdcarey@gmail.com>
2023-12-02 12:30:29 +1100

    add lc 424

---
+++ sliding-window/424-longest-repeating-character-replacement.py
@@ -0,0 +1,29 @@
+# 424. Longest Repeating Character Replacement
+# https://leetcode.com/problems/longest-repeating-character-replacement/description/
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # Substring -> sliding window
+        # Longest -> dynamic
+        # Frequency -> aux data structure
+        # K operations -> hash map
+
+        if not s: return 0
+
+        start = 0
+        max_char = 1
+        freq = {}
+
+        freq[s[start]] = 1
+
+        for end in range(1, len(s)):
+            c = s[end]
+            freq[c] = freq.get(c, 0) + 1
+            max_char = max(max_char, freq[c])
+            if end - start + 1 > max_char + k:
+                freq[s[start]] = freq[s[start]] - 1
+                max_char = max(max_char, freq[s[start]])
+                start += 1  
+
+        if len(s) < max_char + k: return len(s) 
+        else: return max_char + k
