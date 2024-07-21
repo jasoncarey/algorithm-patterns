4711d6a1ef8628ffa5619946d1d15ac8e25128b2
Jason Carey <jasdcarey@gmail.com>
2024-07-21 21:50:48 +1000

    add lc 268

---
+++ interview-practice/268-missing-number.py
@@ -0,0 +1,36 @@
+# 268. Missing Number
+# https://leetcode.com/problems/missing-number/
+
+# TC: O(n)
+# SC: O(n)
+
+from typing import List
+
+class Solution:
+    def missing_number(self, nums: List[int]) -> int:
+        seen = {}
+        for num in nums:
+            seen[num] = True
+        for i in range(len(nums)+1):
+            if i not in seen:
+                return i
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    i_1 = [3, 0, 1]
+    print(s.missing_number(i_1))
+
+    i_2 = [0, 1]
+    print(s.missing_number(i_2))
+
+    i_3 = [9,6,4,2,3,5,7,0,1]
+    print(s.missing_number(i_3))
+
+    i_4 = []
+    print(s.missing_number(i_4))
+
+    i_5 = [1]
+    print(s.missing_number(i_5))
