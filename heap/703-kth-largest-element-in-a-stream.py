141e1cf04924079781af7678af2f127d40d89267
Jason Carey <jasdcarey@gmail.com>
2024-10-12 16:42:22 +1100

    add #703, start heap

---
+++ heap/703-kth-largest-element-in-a-stream.py
@@ -0,0 +1,28 @@
+"""
+703. Kth Largest Element in a Stream
+https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+Easy
+
+TC: O(nlog(k)), O(log(k))
+SC: O(k)
+"""
+
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.h = []
+        self.k = k
+        for i, num in enumerate(nums):
+            if i < k:
+                heapq.heappush(self.h, num)
+            else:
+                heapq.heappushpop(self.h, num)
+
+    def add(self, val: int) -> int:
+        if len(self.h) < self.k:
+            heapq.heappush(self.h, val)
+        else:
+            heapq.heappushpop(self.h, val)
+
+        return self.h[0]
