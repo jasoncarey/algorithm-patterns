579fe6e637bf47be35dd57f6ef4cc3e74a36b6fe
Jason Carey <jasdcarey@gmail.com>
2025-01-27 14:44:03 +1100

    add #211

---
+++ tries/211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,48 @@
+"""
+211. Design Add and Search Words Data Structure
+https://leetcode.com/problems/design-add-and-search-words-data-structure/description/
+
+Medium
+
+Node and add_word is the same as in a Trie
+
+TC: O(n)
+SC: O(t + n)
+"""
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add_word(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isEnd = True
+
+    def search(self, word):
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+
+        return dfs(0, self.root)
