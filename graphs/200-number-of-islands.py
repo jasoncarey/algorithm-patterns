88ce6c5f363326cb68e224b5cfc0d03a42def13b
Jason Carey <jasdcarey@gmail.com>
2025-01-29 16:58:23 +1100

    start graphs, add #200

---
+++ graphs/200-number-of-islands.py
@@ -0,0 +1,32 @@
+"""
+200. Number of Islands
+https://leetcode.com/problems/number-of-islands/description/
+
+Medium
+
+TC: O(m*n)
+SC: O(m*n)
+"""
+
+
+def num_islands(grid):
+
+    ROWS, COLS = len(grid), len(grid[0])
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    islands = 0
+
+    def dfs(r, c):
+        if r < 0 or c < 0 or r >= ROWS or c >= ocls or grid[r][c] == "0":
+            return
+
+        grid[r][c] = "0"
+        for dr, dc in directions:
+            dfs(r + dr, c + dc)
+
+    for r in range(ROWS):
+        for c in range(COLS):
+            if grid[r][c] == "1":
+                dfs(r, c)
+                islands += 1
+
+    return islands
